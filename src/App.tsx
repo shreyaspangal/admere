1344ef6f7b2b7d0b0dbb7d765217ce936faff124
Shreyas Pangal <shreyaspangal03@gmail.com>
2023-01-02 00:12:05 +0530

    Fix: minor fixes

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,17 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Homepage from './pages/Homepage';
+import Login from './pages/Login';
+import ProfileUpload from './pages/ProfileUpload';
+import RegisterForm from './pages/RegisterForm';
 
 function App() {
   return (
     <div className="app">
-      <Homepage />
+      {/* <Homepage /> */}
+      {/* <RegisterForm /> */}
+      {/* <Login /> */}
+      <ProfileUpload />
     </div>
   );
 }
