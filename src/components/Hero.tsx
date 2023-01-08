451ef9ad255421a89a34f67dd1d667f7a10b9d97
Shreyas Pangal <shreyaspangal03@gmail.com>
2023-01-08 21:55:50 +0530

    add UI: add toaster notification banner on top after user logs in

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Hero.module.scss';
+import { Link } from "react-router-dom";
 
 type Assets = {
   heroTitle?: string,
@@ -13,6 +14,8 @@ type Assets = {
 
 function Hero({ heroTitle, heroSubtitle, btnText, onClick, btnIcon, bgHeight, bgInnerHeight }: Assets) {
 
+  const [isLoggedIn, setIsLoggedIn] = React.useState(true);
+
   // const heroStyles = {
   //   height: '600px',
   //   backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.539), rgb(0, 0, 0)), url('../assets/images/hero-img.png')",
@@ -22,8 +25,15 @@ function Hero({ heroTitle, heroSubtitle, btnText, onClick, btnIcon, bgHeight, bg
   // }
 
   return (
-    <div className={styles.hero} style={{height: bgHeight || ''}}>
-      <div className={styles.hero__inner} style={{height: bgInnerHeight || ''}}>
+    <div className={styles.hero} style={{ height: bgHeight || '600px' }}>
+      <div className={styles.hero__inner} style={{ height: bgInnerHeight || '600px' }}>
+        {
+          isLoggedIn &&
+          <div className={styles.hero__toast}>
+            <p>Your now logged in with admere and can submit your video or view your profile <Link to='/profile-upload'>here</Link>. If already submitted please ignore.</p>
+            <button onClick={() => setIsLoggedIn(false)}>X</button>
+          </div>
+        }
         <div className={styles.hero__innerText}>
           {heroTitle && <h2 className='fs-1 mx-auto'>{heroTitle}</h2>}
           {heroSubtitle && <p className='fs-6 w-75 mx-auto my-5 my-lg-4'>{heroSubtitle}</p>}
