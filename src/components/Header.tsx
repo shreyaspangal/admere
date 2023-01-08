cdd906f411cc5e21996a5f90d5dd27e7fa2b2b4f
Shreyas Pangal <shreyaspangal03@gmail.com>
2023-01-08 21:54:49 +0530

    Feat: add go back button in header for pages other than home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
 import './Header.scss';
+import { useNavigate } from 'react-router-dom';
 import { ReactComponent as Logo } from '../assets/images/logo.svg';
 
 function Header() {
 
+    const navigate = useNavigate();
     const [toggleNavMenu, setToggleNavMenu] = React.useState(false);
 
     function handleToggleMenu() {
         setToggleNavMenu(!toggleNavMenu);
     }
 
+    function handleGoback() {
+        navigate(-1);
+    }
+
     const currMenuIcon = toggleNavMenu ? 'opened' : '';
+    const showNavBarLinks = window.location.pathname == '/' || window.location.pathname == '/home' ;
 
     return (
         <nav className='nav'>
             <div className='nav__inner'>
                 <Logo className='nav__logo' />
-                {!toggleNavMenu &&
+                {
+                    (!toggleNavMenu && showNavBarLinks) &&
                     <ul className='nav__links'>
                         <li><a href="#whoWeAre">Who we are</a></li>
                         <li><a href="#howItHelps">How it helps</a></li>
@@ -25,7 +33,8 @@ function Header() {
                         <li><a href="#faqs">Faq's</a></li>
                     </ul>
                 }
-                {toggleNavMenu &&
+                {
+                    (toggleNavMenu && showNavBarLinks) &&
                     <ul className='nav__links-mobile'>
                         <li><a href="#whoWeAre">Who we are</a></li>
                         <li><a href="#howItHelps">How it helps</a></li>
@@ -34,9 +43,18 @@ function Header() {
                         <li><a href="#faqs">Faq's</a></li>
                     </ul>
                 }
-                <button className='nav__contactBtn'>
-                    Contact us
-                </button>
+                {
+                    !showNavBarLinks &&
+                    (<button className='nav__gobackBtn' onClick={handleGoback}>
+                        Go back
+                    </button>)
+                }
+                {
+                    showNavBarLinks &&
+                    (<button className='nav__contactBtn'>
+                        Contact us
+                    </button>)
+                }
                 <button className={`${'nav__hamburger'} ${currMenuIcon}`} onClick={handleToggleMenu}>
                     <span></span>
                 </button>
