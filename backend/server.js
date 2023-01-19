efa9d2ef2edd56d902a883f882360ea767281578
Shreyas Pangal <shreyaspangal03@gmail.com>
2023-01-19 22:28:18 +0530

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const connectDB = require("./config/db");
 const routes = require("./routes");
 const cors = require("cors");
 const { notFound, errorHandler } = require("./middlewares/error.middleware");
+const path = require("path");
 
 const app = express();
 dotenv.config();
@@ -17,6 +18,17 @@ app.use(cors());
 app.options("*", cors());
 
 app.use("/api", routes);
+
+// ------------ deployment ----------------- //
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    })
+}
+// ------------ deployment ----------------- //
 
 // Error handlers
 app.use(notFound);
